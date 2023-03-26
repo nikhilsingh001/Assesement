60396a2d289940a7bff0b506f9360d61fb15afc8
Nikhil Singh <Nikhilsingh.nick@gmail.com>
2023-03-27 01:07:05 +0530

    added new file

---
+++ CustomerData/dbo/customer_report.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[customer_report]
+	@param1 int = 0,
+	@param2 int
+AS
+	begin
+select c.CustomerName,p.Product_Name,Address,ct.CITY_Name,s.State_Name,c.Pincode,c.DOB from customer c inner join Product p on c.ProductID=p.ProductID
+inner join City ct on c.CityID=ct.CityID
+inner join [State] s on c.StateID=s.StateID
+
+end
+
+
---
+++ WebApi/Models/ReportData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public class ReportData
+    {
+        public string Name { get; set; }
+        public List<string> ProducttName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Pincode { get; set; }
+        public DateTime DOB { get; set; }
+    }
+}
